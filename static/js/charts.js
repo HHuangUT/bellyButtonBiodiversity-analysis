2f08c2194612bd72291150b86f9f69ee9c25005e
HHuangUT <huangsterjr+UT@gmail.com>
2021-05-30 14:23:46 -0500

    Pre Deliverable 4

--- static/js/charts.js
+++ static/js/charts.js
@@ -117,7 +117,7 @@ function buildCharts(sample) {
       },
       xaxis: {title: "OTU ID"},
       margin: {
-        l: 75,
+        l: 80,
         r: 50,
         b: 60,
         t: 60,
@@ -128,4 +128,53 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", [bubbleData], bubbleLayout);
+
+    // Deliverable 3
+
+    // Create a variable that holds the samples array. 
+    var metadata = data.metadata;
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var Result = resultArray[0];
+
+    // 3. Create a variable that holds the washing frequency.
+    var wFreq = parseFloat(Result.wfreq);
+    
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = {
+      type: "indicator",
+      value: wFreq,
+      mode: "gauge+number",
+      gauge: {
+        axis: {range: [0,10], dtick: 2},
+        bar: {color: "black"},
+        steps: [
+          {range: [0,2], color: "red"},
+          {range: [2,4], color: "orange"},
+          {range: [4,6], color: "yellow"},
+          {range: [6,8], color: "yellowgreen"},
+          {range: [8,10], color: "green"}
+        ],
+      }
+    };
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      title: {
+        text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+        y: 0.75,
+      },
+      margin: {
+        l: 50,
+        r: 50,
+        b: 0,
+        t: 50,
+        pad: 50
+      },
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", [gaugeData], gaugeLayout);
   })};
